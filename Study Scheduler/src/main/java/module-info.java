ea193efe5e84fc7dc0bec74b8e52fbfe9f317b67
wesnewcomb <newcombw@knights.ucf.edu>
2022-11-15 11:26:23 -0500

    Moved improperly placed class

--- Study Scheduler/src/main/java/module-info.java
+++ Study Scheduler/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.studyscheduler {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.sql;
 
 
     opens com.studyscheduler to javafx.fxml;
